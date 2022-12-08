6d1e9dcb70570ed4bdeabbeb3dc66cb0a2b10678
Simone <coli.simone001@gmail.com>
2022-12-08 20:20:47 +0100

    Correction to the class, now working

--- source/Legendre_Polynomials.py
+++ source/Legendre_Polynomials.py
@@ -3,21 +3,21 @@ import numpy as np
 class Legendre_Polynomials:
     def __init__(self, n, theta, phi):
         self.n = n
-        if n == 1:
+        if n == 0:
             #Y(0,0)
             self.Y = 0.5 * np.sqrt(1 / np.pi) * np.ones(theta.shape)
-        elif n == 2:
+        elif n == 1:
             #Y(0,0)
             self.Y = -0.5 * np.sqrt(1.5 / np.pi) * np.cos(phi) * np.sin(theta)
-        elif n == 3:
+        elif n == 2:
             #Y(0,0)
             self.Y = 0.25 * np.sqrt(7.5 / np.pi) * np.cos(2 * phi) * np.sin(theta)**2
 
     def abs(self):
         return np.abs(self.Y)
-    def max(self):
+    def get_max(self):
         return self.Y.max()
-    def min(self):
+    def get_min(self):
         return self.Y.min()
     def shape(self):
         return self.Y.shape
@@ -26,5 +26,4 @@ class Legendre_Polynomials:
         bool = self.Y.max() == self.Y.min()
         return bool
     def norm(self):
-        if self.Y.max() != self.Y.min():
-            return 2 * (self.Y - self.Y.min())/(self.Y.max()-self.Y.min()) - 1
+        return 2 * (self.Y - self.Y.min())/(self.Y.max()-self.Y.min()) - 1
